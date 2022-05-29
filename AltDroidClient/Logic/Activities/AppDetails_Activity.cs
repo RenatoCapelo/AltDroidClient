2b25210d6560c9ca7e71fedcbf962dad25e7987f
Renato Manuel Branco Capelo Ventura <2219112@alunos.escoladigital.com>
2022-05-29 03:31:41 +0100

    IT WORKS!

--- AltDroidClient/Logic/Activities/AppDetails_Activity.cs
+++ AltDroidClient/Logic/Activities/AppDetails_Activity.cs
@@ -6,12 +6,19 @@ using Android.Views;
 using Android.Widget;
 using AndroidX.AppCompat.App;
 using App1.Models;
+using App1.Logic.Services;
 using Newtonsoft.Json;
 using Square.Picasso;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using System.IO;
+using AndroidX.Core.Content;
+using Xamarin.Essentials;
 
 namespace App1.Logic.Activities
 {
@@ -20,18 +27,29 @@ namespace App1.Logic.Activities
     {
         AppToGet app;
 
+        private ImageView btnBack;
         private TextView title;
         private TextView price;
         private ImageView logo;
+        private Button btnDownload;
 
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override async void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             app = JsonConvert.DeserializeObject<AppToGet>(Intent.GetStringExtra("app"));
             SetContentView(Resource.Layout.AppDetails);
+            var status = await CrossPermissions.Current.CheckPermissionStatusAsync<StoragePermission>();
+            while (status != Plugin.Permissions.Abstractions.PermissionStatus.Granted)
+            {
+                status = await CrossPermissions.Current.RequestPermissionAsync<StoragePermission>();
+            }
+            btnBack = FindViewById<ImageView>(Resource.Id.appDetails_backArrow);
+            btnBack.Click += BtnBack_Click;
             title = FindViewById<TextView>(Resource.Id.appDetails_Title);
             price = FindViewById<TextView>(Resource.Id.appDetails_Price);
             logo = FindViewById<ImageView>(Resource.Id.appDetails_logo);
+            btnDownload = FindViewById<Button>(Resource.Id.appDetails_Download);
+            btnDownload.Click += BtnDownload_Click;
 
             title.Text = app.title;
             price.Text = "Gratuito";
@@ -46,6 +64,47 @@ namespace App1.Logic.Activities
                     .Into(logo);
             }
             // Create your application here
+        }
+
+        private void BtnDownload_Click(object sender, EventArgs e)
+        {
+            var downloader = new FileDownloader();
+            downloader.DownloadFile($"https://api.appstore.renatoventura.pt/storage/apps/{app.developer.devGuid}/{app.applicationGuid}/{app.fileName}",$"{app.applicationGuid}");
+            downloader.OnFileDownloaded += Downloader_OnFileDownloaded;
+        }
+
+        private void Downloader_OnFileDownloaded(object sender, Events.DownloadEventArgs e)
+        {
+            if (e.FileSaved)
+            {
+                Intent appInstall = new Intent(Intent.ActionView);
+                try
+                {
+                    var pathToInstall = new Java.IO.File(Android.App.Application.Context.GetExternalFilesDir("").AbsolutePath, app.applicationGuid.ToString());
+                    var toInstall = new Java.IO.File(pathToInstall, app.fileName);
+
+                    Android.Net.Uri apkUri = AndroidX.Core.Content.FileProvider.GetUriForFile(Application.Context,
+                    AppInfo.PackageName + ".provider",
+                    toInstall);
+                    appInstall.SetDataAndType(apkUri, "application/vnd.android.package-archive");
+                    appInstall.SetFlags(ActivityFlags.GrantReadUriPermission);
+                    StartActivity(appInstall);
+
+                }
+                catch(Exception ex)
+                { 
+                }
+                Toast.MakeText(this, "The app was downloaded!", ToastLength.Long).Show();
+            }
+            else
+            {
+                Toast.MakeText(this,"There was an error downloading the app", ToastLength.Long).Show();
+            }
+        }
+
+        private void BtnBack_Click(object sender, EventArgs e)
+        {
+            this.Finish();
         }
     }
 }
---
+++ AltDroidClient/Logic/Services/FileDownloader.cs
@@ -0,0 +1,59 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using App1.Logic.Events;
+using Flurl.Http;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace App1.Logic.Services
+{
+    internal class FileDownloader
+    {
+        public event EventHandler<DownloadEventArgs> OnFileDownloaded;
+        public void DownloadFile(string url, string folder)
+        {
+            string pathToNewFolder = Path.Combine(Android.App.Application.Context.GetExternalFilesDir("").AbsolutePath, folder);
+            Directory.CreateDirectory(pathToNewFolder);
+
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                string pathToNewFile = Path.Combine(pathToNewFolder, Path.GetFileName(url));
+                webClient.DownloadFileAsync(new Uri(url), pathToNewFile);
+            }
+            catch (Exception ex)
+            {
+                if (OnFileDownloaded != null)
+                    OnFileDownloaded.Invoke(this, new DownloadEventArgs(false,""));
+            }
+        }
+
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                if (OnFileDownloaded != null)
+                    OnFileDownloaded.Invoke(this, new DownloadEventArgs(false,""));
+            }
+            else
+            {
+                if (OnFileDownloaded != null)
+                    OnFileDownloaded.Invoke(this, new DownloadEventArgs(true, Android.App.Application.Context.GetExternalFilesDir("").AbsolutePath));
+            }
+        }
+    }
+}
